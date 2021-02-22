2d6ac1c862280b5cd6ce7bbc15c2abc20f5acb00
Manh Pham <manhpham90vn@icloud.com>
2021-02-22 11:43:55 +0700

    Initial Commit

---
+++ Translator Pro/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Translator Pro
+//
+//  Created by Manh Pham on 2/22/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
